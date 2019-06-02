d2736b7a31841895f96664450d9c2677b5a09fa8
Dishant Pal <dishantpal37@gmail.com>
2019-06-02 23:10:20 +0530

    Initial commit

---
+++ canvas.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+from tkinter import *
+
+from tkinter import messagebox
+
+top = Tk()
+
+C = Canvas(top, bg = "blue", height = 250, width = 300)
+
+coord = 10, 10, 240, 210
+arc = C.create_arc(coord, start = -90, extent = 350, fill = "red")
+C.pack()
+top.mainloop()
+#my comment at the end
---
+++ demo.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from tkinter import ttk
+
+def get_sum(event):
+    sum = int(no1.get()) + int(no2.get())
+    res.delete(0, "end")
+    res.insert(0, sum)
+
+root = Tk()
+
+no1 = Entry(root)
+no1.pack(side=LEFT)
+
+Label(root, text="+").pack(side=LEFT)
+
+no2 = Entry(root)
+no2.pack(side=LEFT)
+
+equals = Button(root, text="=")
+equals.bind("<Button-1>", get_sum)
+equals.pack(side=LEFT)
+
+res = Entry(root)
+res.pack(side=LEFT)
+
+
+
+
+root.mainloop()
